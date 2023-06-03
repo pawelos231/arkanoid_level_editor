7bcdeb65417460ebdcb79777e04491baa292dbf9
pawelLinek <pawellinek3D@gmail.com>
2023-06-03 14:51:22 +0200

    added init menu view

---
+++ src/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import "./navbar.css";
+
+const Navabar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <h1>Level editor Menu</h1>
+        <div className="grid">
+          <p>SIATKA</p>
+        </div>{" "}
+        <div className="kolor">
+          <p>Kolor cegły</p>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navabar;
