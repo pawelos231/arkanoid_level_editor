c738ca7121f9a305f7e000a627709bdb1426c5a1
pawelLinek <pawellinek3D@gmail.com>
2023-06-05 21:45:44 +0200

    minor canvas component improvements

--- src/components/Editor/Canvas.tsx
+++ src/components/Editor/Canvas.tsx
@@ -1,5 +1,4 @@
 import { useRef, useEffect, RefObject, useState, useCallback } from "react";
-import { memo } from "react";
 import { drawBricks, generateBrickGrid } from "../../helpers/generateBrickGrid";
 import { Brick } from "../../helpers/generateBrickGrid";
 import { handleCanvasClick } from "../../helpers/clickBrick";
@@ -19,108 +18,105 @@ interface CanvasProps {
   grid: boolean;
 }
 
-const Canvas = memo(
-  ({
-    width,
-    height,
-    rowsNumber,
-    columnsNumber,
-    brickColor,
-    grid,
-  }: CanvasProps) => {
-    const canvasRef: RefObject<HTMLCanvasElement> = useRef(null);
-    const [bricks, setBricks] = useState<Brick[]>([]);
-    const [canvas, setCanvas] = useState<HTMLCanvasElement>();
-    const [context, setContext] = useState<CanvasRenderingContext2D>();
+const Canvas = ({
+  width,
+  height,
+  rowsNumber,
+  columnsNumber,
+  brickColor,
+  grid,
+}: CanvasProps) => {
+  const canvasRef: RefObject<HTMLCanvasElement> = useRef(null);
+  const [bricks, setBricks] = useState<Brick[]>([]);
+  const [canvasContext, setCanvasContext] = useState<{
+    canvas: HTMLCanvasElement | null;
+    context: CanvasRenderingContext2D | null;
+  }>({ canvas: null, context: null });
 
-    const OUT_OF_BOUNDS =
-      rowsNumber > MAX_ROWS_COUNT ||
-      columnsNumber > MAX_COLUMNS_COUNT ||
-      rowsNumber < 0 ||
-      columnsNumber < 0;
+  const OUT_OF_BOUNDS =
+    rowsNumber > MAX_ROWS_COUNT ||
+    columnsNumber > MAX_COLUMNS_COUNT ||
+    rowsNumber < 0 ||
+    columnsNumber < 0;
 
-    useEffect(() => {
-      if (OUT_OF_BOUNDS) {
-        console.warn(OUT_OF_RANGE);
-        return;
-      }
+  useEffect(() => {
+    if (OUT_OF_BOUNDS) {
+      console.warn(OUT_OF_RANGE);
+      return;
+    }
 
-      const canvas: HTMLCanvasElement | null = canvasRef.current;
-      if (!canvas) return;
-      setCanvas(canvas);
+    const canvas: HTMLCanvasElement | null = canvasRef.current;
+    if (!canvas) return;
+    const context: CanvasRenderingContext2D | null = canvas.getContext("2d");
+    if (!context) return;
 
-      const context: CanvasRenderingContext2D | null = canvas.getContext("2d");
-      if (!context) return;
-      setContext(context);
+    const generatedBricks: Brick[] = generateBrickGrid(
+      canvas,
+      columnsNumber,
+      rowsNumber
+    );
 
-      const generatedBricks: Brick[] = generateBrickGrid(
+    setBricks(generatedBricks);
+    drawBricks(context, canvas, generatedBricks);
+
+    const handleResize = () => {
+      const resizedBricks: Brick[] = generateBrickGrid(
         canvas,
         columnsNumber,
         rowsNumber
       );
+      setBricks(resizedBricks);
+      drawBricks(context, canvas, resizedBricks);
+    };
 
-      setBricks(generatedBricks);
-      drawBricks(context, canvas, generatedBricks);
+    window.addEventListener("resize", handleResize);
 
-      const handleResize = () => {
-        const resizedBricks: Brick[] = generateBrickGrid(
-          canvas,
-          columnsNumber,
-          rowsNumber
-        );
-        setBricks(resizedBricks);
-        drawBricks(context, canvas, resizedBricks);
-      };
+    setCanvasContext({ canvas, context });
 
-      window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [rowsNumber, columnsNumber, OUT_OF_BOUNDS]);
 
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }, [rowsNumber, columnsNumber, OUT_OF_BOUNDS]);
+  useEffect(() => {
+    const { context, canvas } = canvasContext;
+    if (!context || !canvas) return;
+    drawBricks(context, canvas, bricks, grid);
+  }, [grid, bricks, canvasContext]);
 
-    useEffect(() => {
-      if (!context || !canvas) return;
-      drawBricks(context, canvas, bricks, grid);
-    }, [grid, bricks, context, canvas]);
+  type ClickReactEvent = React.MouseEvent<HTMLCanvasElement, MouseEvent>;
 
-    type ClickReactEvent = React.MouseEvent<HTMLCanvasElement, MouseEvent>;
+  const handleClick = useCallback(
+    (e: ClickReactEvent): void => {
+      const canvas: HTMLCanvasElement | null = canvasRef.current;
+      if (!canvas) return;
+      const context: CanvasRenderingContext2D | null = canvas.getContext("2d");
+      if (!context) return;
 
-    const handleClick = useCallback(
-      (e: ClickReactEvent): void => {
-        const canvas: HTMLCanvasElement | null = canvasRef.current;
-        if (!canvas) return;
+      const newBricks: Brick[] = handleCanvasClick(
+        e,
+        bricks.map((brick) => ({ ...brick })), // Create a new array with updated values
+        canvas,
+        brickColor
+      );
+      setBricks(newBricks);
+    },
+    [bricks, canvasRef, brickColor]
+  );
 
-        const context: CanvasRenderingContext2D | null =
-          canvas.getContext("2d");
-
-        if (!context) return;
-
-        const newBricks: Brick[] = handleCanvasClick(
-          e,
-          JSON.parse(JSON.stringify(bricks)),
-          canvas,
-          brickColor
-        );
-        setBricks(newBricks);
-      },
-      [bricks, canvasRef, brickColor]
-    );
-
-    if (rowsNumber <= 0 || columnsNumber <= 0) {
-      return <NoView />;
-    }
-
-    return (
-      <canvas
-        className="main"
-        ref={canvasRef}
-        width={width}
-        height={height}
-        onClick={handleClick}
-      />
-    );
+  if (rowsNumber <= 0 || columnsNumber <= 0) {
+    return <NoView />;
   }
-);
+
+  return (
+    <canvas
+      className="main"
+      ref={canvasRef}
+      width={width}
+      height={height}
+      onClick={handleClick}
+    />
+  );
+};
 
 export default Canvas;
